bb558a43e9dd7bcb73b38513094d9f916aef2110
denisbelenko <denisdel123@>
2024-10-09 21:51:24 +0700

    соеденил путь с эндпоинтом

--- realty_addresses/urls.py
+++ realty_addresses/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
 from realty_addresses.apps import RealtyAddressesConfig
-from realty_addresses.views import AddressMapPointListAPIView
+from realty_addresses.views import AddressMapPointsListAPIView
 
 app_name = RealtyAddressesConfig.name
 
 urlpatterns = [
-    path('map-points/', AddressMapPointListAPIView.as_view(), name='map-points')
+    path('map-points/', AddressMapPointsListAPIView.as_view(), name='map-points')
 ]
