6627ba9753f1c13e8963dd792841ed0414677552
denisbelenko <denisdel123@>
2024-10-14 15:44:30 +0700

    создал эндпоинт для создания жалоб в объявлении

--- complaints/views.py
+++ complaints/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from complaints.serializers import ComplaintsSerializer
+from realty.models import Realty
+
+
+class ComplaintsCreateAPIView(generics.CreateAPIView):
+    serializer_class = ComplaintsSerializer
+
+    def perform_create(self, serializer):
+        owner = self.request.user
+        realty_id = self.request.data.get('realty_id')
+        realty = Realty.objects.get(pk=realty_id)
+        serializer.save(owner=owner, realty=realty_id)
