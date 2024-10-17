ead398ba219d7b3518f7836ca985189938ccaa7a
denisbelenko <denisdel123@>
2024-10-17 12:11:42 +0700

    добавил докстринг к эндпоинту и schema для описания

--- complaints/views.py
+++ complaints/views.py
@@ -1,11 +1,18 @@
+from drf_spectacular.utils import extend_schema
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
 from complaints.serializers import ComplaintsSerializer
 from realty.models import Realty
 
 
+@extend_schema(
+    summary='Создание жалобы (передается в т.ч. id объявления, к которому жалоба '
+            'и в поле description вводится текст жалобы)')
 class ComplaintsCreateAPIView(generics.CreateAPIView):
+    """Endpoint to Create complaints"""
     serializer_class = ComplaintsSerializer
+    permission_classes = [IsAuthenticated]
 
     def perform_create(self, serializer):
         owner = self.request.user
