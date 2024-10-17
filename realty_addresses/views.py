b037f5d7690ac17c5ff559c2a23c0d3b0588fe29
denisbelenko <denisdel123@>
2024-10-17 18:09:08 +0700

    для эндпоинтов GetlistMapPointsAPIView и GetListAnnouncementsInMapPoint добавил докстринг и схему, так же написал queriset, добавил сериализаторы для схемы и удалил старый код

--- realty_addresses/views.py
+++ realty_addresses/views.py
@@ -7,7 +7,9 @@ from rest_framework.views import APIView
 from realty.filters import RealtyFilter
 from realty.models import Realty
 from realty.pagination import LimitRealtyPagination
-from realty_addresses.serializers import MapPointsSerializer, GetAnnouncementsInMapPoint
+from realty_addresses.serializers import MapPointsSerializer, GetAnnouncementsInMapPoint, MapPointsRequestSerializer, \
+    GetAnnouncementsInMapPointRequestSerializer
+from config import constants
 
 
 @extend_schema(
@@ -16,20 +18,21 @@ from realty_addresses.serializers import MapPointsSerializer, GetAnnouncementsIn
             'подать кардинаты верхнего левого угла и нижнего правого угла карты для вывода списка точек на карте '
             'а так же передать параметры для фильтрации (опционально)')
 class GetlistMapPointsAPIView(APIView):
+    """ Get list realty's point in map"""
+    queryset = Realty.objects.all().filter(
+        realty_status__status=constants.ADVERTISMENT_STATUS
+    ).order_by('-published_at')
     filter_backends = (DjangoFilterBackend,)
     filterset_class = RealtyFilter
 
     def post(self, request, *args, **kwargs):
+        serializer = MapPointsRequestSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
         top_left_latitude = request.data.get('top_left_latitude')
         top_left_longitude = request.data.get('top_left_longitude')
         bottom_right_latitude = request.data.get('bottom_right_latitude')
         bottom_right_longitude = request.data.get('bottom_right_longitude')
-
-        if (top_left_latitude is None or top_left_longitude is None or bottom_right_latitude is None or
-                bottom_right_longitude is None):
-            return Response(
-                {"error": "Пожалуйста, укажите все необходимые данные!"},
-                status=status.HTTP_400_BAD_REQUEST)
 
         queryset = Realty.objects.filter(
             Q(address__latitude__gte=bottom_right_latitude) &
@@ -45,16 +48,25 @@ class GetlistMapPointsAPIView(APIView):
 
         queryset = filterset.qs
 
-        serializer = MapPointsSerializer(queryset, many=True)
+        response_serializer = MapPointsSerializer(queryset, many=True)
 
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(response_serializer.data, status=status.HTTP_200_OK)
 
 
+@extend_schema(
+    summary='Получение списка объявлений по заданной точке на карте ')
 class GetListAnnouncementsInMapPoint(APIView):
+    """Get List realty in point"""
+    queryset = Realty.objects.all().filter(
+        realty_status__status=constants.ADVERTISMENT_STATUS
+    ).order_by('-published_at')
     filter_backends = (DjangoFilterBackend,)
     filterset_class = RealtyFilter
 
     def post(self, request, *args, **kwargs):
+        serializer = GetAnnouncementsInMapPointRequestSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
         latitude = request.data.get('latitude')
         longitude = request.data.get('longitude')
 
@@ -78,47 +90,3 @@ class GetListAnnouncementsInMapPoint(APIView):
         serializer = GetAnnouncementsInMapPoint(queryset, many=True)
 
         return Response(serializer.data, status=status.HTTP_200_OK)
-
-# @extend_schema(
-#     summary='Получение списка точек на карте в которых есть объявления '
-#             'с заданными параметрами, в заданном прямоугольнике')
-# class AddressMapPointsListAPIView(generics.ListAPIView):
-#     """Get a list of Realties coordinates."""
-#
-#     serializer_class = MapPointsSerializer
-#     # pagination_class = LimitRealtyPagination
-#     filter_backends = (DjangoFilterBackend,)
-#     filterset_class = RealtyFilter
-#
-#     def get_queryset(self, *args, **kwargs):
-#         # Получаем параметры из запроса
-#         try:
-#             top_left_latitude = self.request.data['top_left_latitude']
-#             top_left_longitude = self.request.data['top_left_longitude']
-#             bottom_right_latitude = self.request.data['bottom_right_latitude']
-#             bottom_right_longitude = self.request.data['bottom_right_longitude']
-#         except KeyError:
-#             return Realty.objects.none()
-#
-#         # Фильтруем адреса, которые попадают в заданный прямоугольник
-#         queryset = Realty.objects.filter(
-#             Q(address__latitude__gte=bottom_right_latitude) &
-#             Q(address__latitude__lte=top_left_latitude) &
-#             Q(address__longitude__gte=top_left_longitude) &
-#             Q(address__longitude__lte=bottom_right_longitude)
-#         )
-#
-#         filtered_realty_queryset = self.filterset_class(self.request.data, queryset=queryset).qs
-#
-#         return filtered_realty_queryset
-#
-#     def get(self, request, *args, **kwargs):
-#         queryset = self.get_queryset()
-#
-#         if not queryset.exists():
-#             return Response(
-#                 {"error": "Пожалуйста, укажите все необходимые данные!"},
-#                 status=status.HTTP_400_BAD_REQUEST
-#             )
-#
-#         return self.list(request, *args, **kwargs)
