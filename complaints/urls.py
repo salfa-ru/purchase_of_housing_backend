2e13a6a7e20b11b3e6d540acb8a347064de197e5
denisbelenko <denisdel123@>
2024-10-17 12:10:29 +0700

    изменил путь ссылки удалив create/

--- complaints/urls.py
+++ complaints/urls.py
@@ -6,5 +6,5 @@ from complaints.views import ComplaintsCreateAPIView
 app_name = ComplaintsConfig.name
 
 urlpatterns = [
-    path('create/', ComplaintsCreateAPIView.as_view(), name='create-complaints')
+    path('', ComplaintsCreateAPIView.as_view(), name='create-complaints')
 ]
