d2ec11b05ab81c386d68d7f5e7b643dd749b90be
denisbelenko <denisdel123@>
2024-10-17 13:59:20 +0700

    добавил в сериализатор поле realty_id для передачи индификатора realty

--- complaints/serializers.py
+++ complaints/serializers.py
@@ -1,9 +1,16 @@
 from rest_framework import serializers
 
 from complaints.models import Complaint
+from realty.models import Realty
 
 
 class ComplaintsSerializer(serializers.ModelSerializer):
+    realty_id = serializers.PrimaryKeyRelatedField(
+        queryset=Realty.objects.all(),
+        source='realty',
+        write_only=True
+    )
+
     class Meta:
         model = Complaint
         fields = ['realty_id', 'description']
