f640421e87226811088a2c03f1926aed756c7dec
denisbelenko <denisdel123@>
2024-10-18 15:06:51 +0700

    добавил ссылку для эндпоинта ComplaintsListAPIView

--- complaints/urls.py
+++ complaints/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 from complaints.apps import ComplaintsConfig
-from complaints.views import ComplaintsCreateAPIView
+from complaints.views import ComplaintsCreateAPIView, ComplaintsListAPIView
 
 app_name = ComplaintsConfig.name
 
 urlpatterns = [
-    path('', ComplaintsCreateAPIView.as_view(), name='create-complaints')
+    path('', ComplaintsCreateAPIView.as_view(), name='create-complaints'),
+    path('get-complaints/', ComplaintsListAPIView.as_view(), name='list-complaints')
 ]
