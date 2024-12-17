1322da953d30acb828db65d7254885e482406787
Константин Стеблев <konstantin.sks85@gmail.com>
2024-12-17 11:01:03 +0100

    todo_ursl

--- config/urls.py
+++ config/urls.py
@@ -27,3 +27,8 @@ if DEBUG:
     urlpatterns += [
         path('token-auth/', views.obtain_auth_token)
     ]
+
+# if settings.DEBUG:
+#     urlpatterns += static(
+#         settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+#     )
