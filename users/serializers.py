9d6aecb8c88960c6cd92af330c71b7614482d052
denisbelenko <denisdel123@>
2025-01-13 16:22:39 +0700

    добавил номер телефона и код в UserDataSerializer

--- users/serializers.py
+++ users/serializers.py
@@ -150,6 +150,8 @@ class UserDataSerializer(UserBaseSerializer):
                   'last_name',
                   'registered_for',
                   'avatar',
+                  "phone_number",
+                  "phone_qr_code",
                   )
 
     def get_registered_for(self, obj):
