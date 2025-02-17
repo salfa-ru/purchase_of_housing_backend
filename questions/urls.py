2bc43a8b4e7e5aacc9ff5db6316b5a72699d3b60
PhilSigin <lavdanskie@gmail.com>
2025-02-17 17:55:50 +0100

    /questions/ root endpoint shows all sections/questions/answers

--- questions/urls.py
+++ questions/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
 from questions.apps import QuestionsConfig
-from questions.views import QuestionSectionListAPIView, QuestionSectionRetrieveAPIView
+from questions.views import QuestionSectionListAPIView, QuestionSectionRetrieveAPIView, AllQuestionsListAPIView
 
 app_name = QuestionsConfig.name
 
 urlpatterns = [
     path('sections/', QuestionSectionListAPIView.as_view(), name='sections'),
     path('sections/<pk>', QuestionSectionRetrieveAPIView.as_view(), name='questions'),
+    # новый эндпойт который показывает все вопросы и ответы сразу
+    path('', AllQuestionsListAPIView.as_view(), name='all_questions'),
 ]
