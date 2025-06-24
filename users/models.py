6e111692a368f59db6dd25359abced70e1cd7792
PhilSigin <lavdanskie@gmail.com>
2025-06-24 12:56:12 +0200

    name length 50->40

--- users/models.py
+++ users/models.py
@@ -43,9 +43,9 @@ class User(AbstractUser):
     REQUIRED_FIELDS = []
 
     first_name = models.CharField(verbose_name='Имя',
-                                  max_length=constants.CHAR_LENGTH)
+                                  max_length=constants.NAME_LENGTH)
     last_name = models.CharField(verbose_name='Фамилия',
-                                 max_length=constants.CHAR_LENGTH,
+                                 max_length=constants.NAME_LENGTH,
                                  **constants.NULLABLE_FIELD)
     email = models.EmailField(verbose_name='email', unique=True)
 
