79bbe4b1307a0ff8b18242ccbf68fed8a7e0a7eb
PhilSigin <lavdanskie@gmail.com>
2025-12-16 08:59:25 +0100

    admin is changed - to show realty id for both "Shows"

--- realty_displays/admin.py
+++ realty_displays/admin.py
@@ -5,11 +5,23 @@ from realty_displays import models
 
 @admin.register(models.DisplayInSearch)
 class DisplayInSearchAdmin(admin.ModelAdmin):
-    list_display = ('count', 'realty',)
+    list_display = ('count', 'realty', 'realty_id',)
     list_display_links = ('count',)
+
+    def realty_id(self, obj):
+        """Returns the realty ID."""
+        return obj.realty.pk if obj.realty else None
+    realty_id.short_description = 'ID недвижимости'
+    realty_id.admin_order_field = 'realty__id'
 
 
 @admin.register(models.DisplayFullInfo)
 class DisplayFullInfoAdmin(admin.ModelAdmin):
-    list_display = ('date', 'count', 'realty',)
+    list_display = ('date', 'count', 'realty', 'realty_id',)
     list_display_links = ('date', 'count',)
+
+    def realty_id(self, obj):
+        """Returns the realty ID."""
+        return obj.realty.pk if obj.realty else None
+    realty_id.short_description = 'ID недвижимости'
+    realty_id.admin_order_field = 'realty__id'
