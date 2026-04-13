2fc19dd106ee6b9ff5434d502895197b7995dd74
Nazir Valeev <nazirvaleev@gmail.com>
2026-04-13 22:18:01 +0300

    Changed settings.py.

--- config/settings.py
+++ config/settings.py
@@ -6,6 +6,7 @@ load_dotenv()
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DATA_DIR = os.path.join(BASE_DIR, 'data')
+os.makedirs(DATA_DIR, exist_ok=True)
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'default_key')
 
