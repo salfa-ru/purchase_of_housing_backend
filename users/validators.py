9c34e8dce8a47fb0a0d1f94ce372ea1dcaf2aa23
Anastasiia Puzacheva <anastasiapuzaceva@gmail.com>
2026-07-19 21:09:30 +0200

    Добавлен валидатор пароля

--- users/validators.py
+++ users/validators.py
@@ -1,3 +1,6 @@
+import re
+
+from django.core.exceptions import ValidationError
 from django.core.validators import RegexValidator
 
 validate_person_name = RegexValidator(
@@ -12,3 +15,22 @@ validate_phone_number = RegexValidator(
     regex=r'^(\+7|8)\d{10}$',
     message='Введите номер телефона в формате +7XXXXXXXXXX или 8XXXXXXXXXX.',
 )
+
+
+class LetterAndDigitPasswordValidator:
+    """Пароль должен содержать минимум одну латинскую букву и одну цифру."""
+
+    def validate(self, password, user=None):
+        if not re.search(r'[A-Za-z]', password):
+            raise ValidationError(
+                'Пароль должен содержать хотя бы одну латинскую букву.',
+                code='password_no_letter',
+            )
+        if not re.search(r'[0-9]', password):
+            raise ValidationError(
+                'Пароль должен содержать хотя бы одну цифру.',
+                code='password_no_digit',
+            )
+
+    def get_help_text(self):
+        return 'Пароль должен содержать минимум одну латинскую букву и одну цифру.'
